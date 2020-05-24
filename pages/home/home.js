159cf9d341ea57bdf4878f53253894429ff15347
wuyong <w971189868@163.com>
2020-05-24 20:08:11 +0800

    事件处理详解

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,50 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    titles: ["衣服","鞋子","内衣"]
+  },
+  handleBtnClick(){
+    console.log("我被点击了")
+  },
+  handleTouchStart(event){
+    console.log("handleTouchStart手指触摸动作开始",event)
+  },
+  handleTouchMove(event){
+    console.log("handleTouchMove手指触摸后移动",event)
+  },
+  handleTouchEnd(event){
+    console.log("handleTouchEnd手指触摸动作结束",event)
+  },
+  handleTap(event){
+    console.log("handleTap手指触摸后马上离开",event)
+  },
+  handleLongpress(event){
+    console.log("handleLongpress手指长按",event)
+  },
+  handleItemClick(event){
+    const dataset = event.currentTarget.dataset;
+    const index = dataset.index
+    const item = dataset.item
+    console.log(index + " "+item)
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 事件的捕获与冒泡
+  handleCaptureView1(){
+    console.log("handleCaptureView1")
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleCaptureView2(){
+    console.log("handleCaptureView2")
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleCaptureView3(){
+    console.log("handleCaptureView3")
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleView1(){
+    console.log("handleView1")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleView2(){
+    console.log("handleView2")
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  handleView3(){
+    console.log("handleView3")
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
