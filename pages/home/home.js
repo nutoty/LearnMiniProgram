78c6165beb6eff6026ae0dc9076537d6f67b40b2
wuyong <w971189868@163.com>
2020-05-25 16:23:20 +0800

    网络请求以及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,47 @@
-// pages/home/home.js
-Page({
+import request from "../../services/request.js"
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    //1.普通的请求方式get
+    // this.get_data_origin()
+    // wx:wx.request({
+    //   url: 'http://152.136.185.210:8000/api/n3/category',
+    //   complete: (res) => {
+    //     console.log(res)
+    //   },
+    // })
+    //2.post请求的方式
+    // wx:wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: "POST",
+    //   data:{
+    //     name: "wuyong",
+    //     age: 18
+    //   },
+    //   success: function(res){
+    //     console.log(res)
+    //   }
+    // })
+
+    //2.封装好的网络请求方式
+    request({
+      url: 'http://152.136.185.210:8000/api/n3/category'
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  get_data_origin(){
+    wx:wx.request({
+      url: 'http://152.136.185.210:8000/api/n3/category',
+      complete: (res) => {
+        console.log(res)
+      },
+    })
   }
 })
---
+++ services/request.js
@@ -0,0 +1,28 @@
+// 向外暴露一个方法
+export default function request(options){
+  return new Promise((resolve,reject) => {
+    wx.request({
+      url: options.url || "",
+      method: options.method || "get",
+      data: options.data || {},
+      success: resolve,
+      fail: reject
+    })
+  })
+}
+//写法不够优雅
+// export default function request(options){
+//   return new Promise((resolve,reject) => {
+//     wx.request({
+//       url: options.url || "",
+//       method: options.method || "get",
+//       data: options.data || {},
+//       success: function(res){
+//         resolve(res)
+//       },
+//       fail: function(err){
+//         reject(err)
+//       }
+//     })
+//   })
+// }
